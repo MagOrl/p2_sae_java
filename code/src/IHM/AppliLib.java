3eee75c9ed5612063c249f3aff02e6ce1a89fd62
mag <m4g0o0@proton.me>
2025-06-16 17:32:52 +0200

    avancement de l'implémentation de l'acceuil

--- code/src/IHM/AppliLib.java
+++ code/src/IHM/AppliLib.java
@@ -1,5 +1,6 @@
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -20,22 +21,31 @@ public class AppliLib extends Application {
             "-fx-text-fill: white;";
     public static String styleBanniere = "-fx-border-radius: 15;" +
             "-fx-border-color:rgb(0, 0, 0);" +
-            "-fx-border-width: 3;"+"-fx-background-color: #0b7f94;" +"-fx-border-insets: -2.5px;" ;
+            "-fx-border-width: 3;" + "-fx-background-color: #0b7f94;" + "-fx-border-insets: -2.5px;";
+    public static String styleDefaultContainer = "-fx-background-color: #5d9b7d;" +
+            "-fx-border-radius: 20; " +
+            "-fx-background-radius: 20;" +
+            "-fx-border-color:rgb(32, 33, 32);" +
+            "-fx-border-width: 2;" +
+            "-fx-text-fill: white;";
 
     @Override
     public void init() {
         this.btnQuitte = new Button("Quitter");
         this.connexion = new Button("Connexion");
         this.creeCompte = new Button("Créer compte");
-        this.menuAcc = new MenuAcceuil(this.btnQuitte, this.connexion, this.creeCompte);
         this.btnQuitte.setStyle(styleBouton);
         this.connexion.setStyle(styleBouton);
         this.creeCompte.setStyle(styleBouton);
-        this.connexion.setMinHeight(40);        
+        this.connexion.setMinHeight(40);
         this.connexion.setMinWidth(90);
-
         this.btnQuitte.setMinHeight(40);
         this.btnQuitte.setMinWidth(90);
+        this.creeCompte.setMinHeight(40);
+        this.creeCompte.setMinWidth(90);
+        this.menuAcc = new MenuAcceuil(this.btnQuitte, this.connexion, this.creeCompte);
+        this.setPadding(new Insets(10));
+
     }
 
     @Override
@@ -47,7 +57,6 @@ public class AppliLib extends Application {
     }
 
     public void afficheMenuAcceuil() {
-        this.scene.setRoot(new MenuAcceuil(this.btnQuitte, this.connexion, this.creeCompte));
+        this.scene.setRoot(this.menuAcc);
     }
-
 }
--- code/src/IHM/MenuAcceuil.java
+++ code/src/IHM/MenuAcceuil.java
@@ -1,11 +1,18 @@
 
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -15,49 +22,62 @@ public class MenuAcceuil extends BorderPane {
     private Button btnQuitte;
     private Button creeCompte;
     private Button connexion;
+    private TextField tfID;
+    private PasswordField pf;
+    private ComboBox<String> cb;
 
     public MenuAcceuil(Button btnQuitte, Button creeCompte, Button connexion) {
+        this.tfID = new TextField();
+        this.pf = new PasswordField();
+        this.cb = new ComboBox<>();
         this.btnQuitte = btnQuitte;
         this.connexion = connexion;
         this.creeCompte = creeCompte;
+        ImageView logo = new ImageView("../img/PileOfBooks.png");
+        logo.setFitHeight(400);
+        logo.setFitWidth(200);
         this.setTop(top());
         this.setLeft(left());
-        this.setRight(right());
+        this.setRight(logo);
+
     }
 
     public BorderPane top() {
         BorderPane bp = new BorderPane();
-        // bp.setFillHeight(false);
-
-        Text txt = new Text("Livre Expresss");
-        txt.setFont(Font.font("Arial", 30));
+        Text txt = new Text("Livre Express");
+        txt.setFont(Font.font("Arial", FontWeight.BOLD, 30));
         ImageView logo = new ImageView("../img/logo_placeholder.png");
         logo.setFitHeight(48);
         logo.setFitWidth(48);
-
         HBox hbleft = new HBox(10);
         HBox hbright = new HBox(10);
         hbleft.getChildren().addAll(txt, logo);
         hbright.getChildren().addAll(this.creeCompte, this.btnQuitte);
         bp.setLeft(hbleft);
         bp.setRight(hbright);
-
         bp.setStyle(AppliLib.styleBanniere);
         bp.setPadding(new Insets(20));
-
         return bp;
     }
 
-    public Pane center() {
-        return new Pane();
-    }
+    public GridPane left() {
+        GridPane gp = new GridPane();
+        Text txt = new Text("Se connecter"); // c r c r
+        gp.add(new Label("Identifiant : "), 0, 1);
+        gp.add(new Label("Mot de passe : "), 0, 2);
+        gp.add(new Label("Compte : "), 0, 3);
+        gp.add(txt, 1, 0, 6, 1);
+        gp.add(this.tfID, 1, 1);
+        gp.add(this.pf, 1, 2);
+        gp.add(this.cb, 1, 3);
+        gp.add(this.connexion, 1, 4, 6, 1);
+        gp.setStyle(AppliLib.styleDefaultContainer);
+        gp.setPadding(new Insets(20));
+        gp.setVgap(20.0);
+        gp.setHgap(50.0);
 
-    public Pane left() {
-        return new Pane();
-    }
+        return gp;
 
-    public Pane right() {
-        return new Pane();
     }
 
 }
