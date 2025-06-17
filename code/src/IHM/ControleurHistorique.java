fff3ab34ec9bb73e17c37e29f9235a6423a68d44
ZDrame28 <o22403796@or-iut-i202-f08.iut45.univ-orleans.fr>
2025-06-17 09:34:24 +0200

    Création de la classe ControleurHistorique.java

--- code/src/IHM/ControleurHistorique.java
+++ code/src/IHM/ControleurHistorique.java
@@ -5,23 +5,15 @@ import javafx.scene.control.Button;
 
 public class ControleurHistorique implements EventHandler<ActionEvent>{
     
-    private MenuClient menu;
     private AppliLib appli;
 
-    public ControleurHistorique(MenuClient menu, Button btnHist, AppliLib appli) {
-        this.menu = menu;
+    public ControleurHistorique(AppliLib appli) {
         this.appli = appli;
     }
 
     @Override
     public void handle(ActionEvent event) { 
-        
-        if (this.menu.instanceOf(MenuHistorique) || this.menu.instanceOf(MenuInfosPersos)) {
-          
-            this.appli.afficheMenuClient();
-        }else{
-            this.appli.afficheMenuVendeur();
-        }
+        this.appli.afficheHistorique();
     }
 
 }
