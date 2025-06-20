1d38cf5015ceaa46432b35ac682c206874349a36
mag <m4g0o0@proton.me>
2025-06-20 09:52:55 +0200

    javadoc

--- code/exec.sh
+++ code/exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-javadoc -d doc --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls -charset utf8 -noqualifier all src/*.java
+javadoc -d doc --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls -charset utf8 -noqualifier all @.sources.txt
 find -name "*.java" >.sources.txt
 javac -d bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls @.sources.txt
 java -cp bin/:/usr/share/java/mariadb-java-client.jar:img/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls AppliLib
